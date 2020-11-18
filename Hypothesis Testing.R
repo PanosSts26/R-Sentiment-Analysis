45a6e467ce900917b2cc804c0520de961319ce5d
PanosSts26 <58661304+PanosSts26@users.noreply.github.com>
2020-11-18 17:51:27 +0200

    Update Hypothesis Testing.R

--- Hypothesis Testing.R
+++ Hypothesis Testing.R
@@ -3,25 +3,28 @@ library(magrittr)
 library(readxl)
 exdata<- read_excel("Data.xlsx")
 Reviews_df<-data.frame(exdata)
+
+#range
+range(Reviews_df$Length)
+range(Reviews_df$Ratings)
+range(Reviews_df$Positive)
+range(Reviews_df$Negative)
+range(Reviews_df$Difference)
+
 #mean
 mean(Reviews_df$Length)
 mean(Reviews_df$Ratings)
 mean(Reviews_df$Positive)
 mean(Reviews_df$Negative)
-#variance.
-var(GLC_df$GLC_partIV)
+mean(Reviews_df$Difference)
+
 #standard deviation.
-sd(GLC_df$GLC_partIV)
+sd(Reviews_df$Length)
+sd(Reviews_df$Ratings)
+sd(Reviews_df$Positive)
+sd(Reviews_df$Negative)
+sd(Reviews_df$Difference)
 
-# ANOVA
-res.aov <- aov(GLC_df$GLC_partII ~ GLC_df$GLC_L1,  data=GLC_df)
-
-# "Opening" the ANOVA model object.
-summary(res.aov)
-
-# coefficients
-coef(PosReg)  
-coef(NegReg)
 
 #regression
 PosReg<-lm(Ratings ~ Positive, data=Reviews_df)
@@ -29,6 +32,7 @@ summary(PosReg)
 NegReg<-lm(Ratings ~ Negative, data=Reviews_df)
 summary(NegReg)
 LengReg<-lm(Ratings~ Length, data = Reviews_df)
+
 #plots
 plot(Ratings ~ Positive, data = Reviews_df)
 abline(PosReg) 
